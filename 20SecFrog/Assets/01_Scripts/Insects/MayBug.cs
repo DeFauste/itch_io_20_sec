85215514c82a85c6c9ca7c1427a801c0994a2477
Vit <banduravit@gmail.com>
2025-11-24 15:17:15 +0300

    фиксы, реворк мухи + мувинг осы

--- 20SecFrog/Assets/01_Scripts/Insects/MayBug.cs
+++ 20SecFrog/Assets/01_Scripts/Insects/MayBug.cs
@@ -4,30 +4,47 @@ using Random = UnityEngine.Random;
 
 public class MayBug : InsectBase
 {
-    private Vector3 _currentDirection;
-    private float _offsetY = 1.6f;
-    void Start()
+    [SerializeField] private float _frequencyY;
+    [SerializeField] private float _offsetY;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _sideSpeedMultiplier; 
+
+    private float _direction;
+    private Vector3 _center;
+
+    private void Start()
     {
-        _speed += Random.Range(-0.7f, 0.7f);
-        _currentDirection = Vector3.right;
+        SetDirection();
+        transform.localScale = Vector3.one * Random.Range(0.8f, 1.2f);
+        _center = transform.position;
+
         StartCoroutine(Move());
     }
+
+    private void SetDirection()
+    {
+        _direction = transform.position.x > 0 ? -1 : 1;
+    }
+
     private IEnumerator Move()
     {
         float angle = Random.Range(0f, 2 * Mathf.PI);
+
         while (true)
         {
-            angle += Time.deltaTime;
-            _currentDirection = new Vector3(1f, Mathf.Cos(angle) * _offsetY, 0f);
-            transform.Translate(_currentDirection * _speed * Time.deltaTime);
+            angle += Time.deltaTime * 2 * Mathf.PI * _speed;
+
+            float y = Mathf.Sin(angle * _frequencyY) * _radius * _offsetY;
+            Vector3 newPosition = new Vector3(0f, y, 0f);
+            transform.position = _center + newPosition;
+            _center += new Vector3(_speed * _sideSpeedMultiplier * _direction, 0f, 0f) * Time.deltaTime;
 
             if (Mathf.Abs(transform.position.x) > 15f || Mathf.Abs(transform.position.y) > 15f)
             {
                 gameObject.SetActive(false);
             }
-            
+
             yield return null;
-          
         }
     }
 }
